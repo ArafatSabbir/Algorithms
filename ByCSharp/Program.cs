0ef8ba598d1f175f1bd9e1f5241a7fa7550a6cb3
Sabbir <sabbirahmedcse33@gmail.com>
2020-05-27 22:46:32 +0600

    Migratory Birds By C#

---
+++ ByCSharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+namespace ByCSharp
+{
+    
+
+    class Solution
+    {
+
+        // Complete the migratoryBirds function below.
+        
+            static int MigratoryBirds(int[] ar)
+            {
+                var birdTypeCounts = new int[5];
+
+                for (int i = 0; i < ar.Length; i++)
+                    birdTypeCounts[ar[i] - 1]++;
+
+                var maxBirdTypeCount = birdTypeCounts[0];
+                var maxBirdType = 1;
+
+                for (int i = 1; i < 5; i++)
+                {
+                    if (birdTypeCounts[i] > maxBirdTypeCount)
+                    {
+                        maxBirdTypeCount = birdTypeCounts[i];
+                        maxBirdType = i + 1;
+                    }
+
+                    if (birdTypeCounts[i] == maxBirdTypeCount && i + 1 < maxBirdType)
+                        maxBirdType = i + 1;
+                }
+
+                return maxBirdType;
+            }
+
+
+
+        static void Main(String[] args)
+        {
+            //No need to capture the size of array. I use array's length property instead.
+            ReadLine();
+            var ar_temp = ReadLine().Split(' ');
+            var ar = Array.ConvertAll(ar_temp, int.Parse);
+            var result = MigratoryBirds(ar);
+            WriteLine(result);
+        }
+    }
+
+}

fdf309ae45833261d981c178dc66f6e05c9aeb35
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-07-16 15:27:02 +0600

    Taum and B'day

---
+++ HackerRank Algorithm/Python/Taum and B'day.py
@@ -0,0 +1,20 @@
+t = int(input().strip())
+
+for t_itr in range(t):
+    first_multiple_input = input().rstrip().split()
+
+    b = int(first_multiple_input[0])
+
+    w = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    bc = int(second_multiple_input[0])
+
+    wc = int(second_multiple_input[1])
+
+    z = int(second_multiple_input[2])
+
+    print( b*min(bc, wc+z) + w*min(wc,bc+z))
+
+

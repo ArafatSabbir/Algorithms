2a0e2ec498381541d5a736d2f2d2e7cd4eead94a
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-07-19 03:42:50 +0600

    Update and all test case passed

--- HackerRank Algorithm/Python/Fair Rations.py
+++ HackerRank Algorithm/Python/Fair Rations.py
@@ -1,33 +1,16 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the fairRations function below.
 def fairRations(B):
-    count = 0
-    s = 0
-    for i in range(len(B)):
-        s += int(B[i])
-        
-        if s % 2 == 1:
-            count +=2
-
-    return count if count%2 == 0 else "NO"
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    N = int(input())
-
-    B = list(map(int, input().rstrip().split()))
-
-    result = fairRations(B)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    B = [i%2 for i in B]
+    c = 0 
+    for i in range(len(B)-1):
+        if B[i] and B[i+1]:
+            B[i] = 0
+            B[i+1] = 0
+            c+=1
+        elif B[i] and not B[i+1]:
+            B[i] = 0
+            B[i+1] = 1
+            c+=1
+    return "NO" if any(B) else c*2
+input()
+B = list(map(int,input().split()))
+print(fairRations(B))

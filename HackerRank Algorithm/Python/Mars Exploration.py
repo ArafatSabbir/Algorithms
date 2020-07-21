2bca504622a9be5bc591668daf0f64b334c7cfcf
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-07-22 00:51:05 +0600

    Mars Exploration py Update

--- HackerRank Algorithm/Python/Mars Exploration.py
+++ HackerRank Algorithm/Python/Mars Exploration.py
@@ -1,22 +1,2 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the marsExploration function below.
-def marsExploration(s):
-    return sum(s[i] != "SOS"[i%3] for i in range(len(s)))
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    s = input()
-
-    result = marsExploration(s)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+s=input().strip()
+print(sum(1 for i in range(len(s)) if s[i]!="SOS"[i%3]))

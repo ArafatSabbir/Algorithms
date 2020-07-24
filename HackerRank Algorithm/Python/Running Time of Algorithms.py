aa8c6af65d38ba267c19c4eb54b1e107d597d12f
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-07-24 22:18:47 +0600

    Update Running Time of Algorithms

--- HackerRank Algorithm/Python/Running Time of Algorithms.py
+++ HackerRank Algorithm/Python/Running Time of Algorithms.py
@@ -1,33 +1,14 @@
-#!/bin/python3
+m = int(input().strip())
+A = [int(i) for i in input().strip().split()]
 
-import math
-import os
-import random
-import re
-import sys
+N= len(A)
+total = 0
+for i in range(1, N):
+    cnt = 0
+    while A[i-1] > A[i] and i >0:
+        A[i], A[i-1] = A[i-1], A[i]
+        i-=1
+        cnt+=1
+    total +=cnt
 
-# Complete the runningTime function below.
-def runningTime(A):
-    N= len(A)
-    total = 0
-    for i in range(1, N):
-        cnt = 0
-        while A[i-1] > A[i] and i >0:
-            A[i], A[i-1] = A[i-1], A[i]
-            i-=1
-            cnt+=1
-        total +=cnt
-    return total
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    result = runningTime(arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+print(total) 

6a1650a868d8da8dc589fcbc4b67e93c636b0dbf
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-07-26 20:56:42 +0600

    The Love-Letter Mystery 2

--- HackerRank Algorithm/Python/The Love-Letter Mystery.py
+++ HackerRank Algorithm/Python/The Love-Letter Mystery.py
@@ -1,25 +1,2 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the theLoveLetterMystery function below.
-def theLoveLetterMystery(s):
-    return sum([abs(ord(s[i])-ord(s[-i-1])) for i in range(len(s)//2)])
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    q = int(input())
-
-    for q_itr in range(q):
-        s = input()
-
-        result = theLoveLetterMystery(s)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+for i in range(int(input())): 
+    print((lambda w: sum(abs(ord(w[i]) - ord(w[len(w)-i-1])) for i in range(len(w)//2)))(input()))

35afcb4a380a040cbf5b27e1feb98cdf3831b101
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-08-12 16:30:59 +0600

    Good Triplets v1

---
+++ LeetCode/Good Triplets.py
@@ -0,0 +1,21 @@
+# 376 ms, faster than 93.81%
+# Memory Usage: 13.9 MB, less than 42.75%
+
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        count = 0
+        l = len(arr)
+    
+        for i in range(0, l - 2):
+
+            for j in range(i + 1, l - 1):
+
+                if(abs(arr[i] - arr[j]) <= a):
+
+                    for k in range(j + 1, l):
+
+                        if((abs(arr[j] - arr[k]) <= b) and (abs(arr[i] - arr[k]) <= c)):
+
+                            count += 1
+                    
+        return count

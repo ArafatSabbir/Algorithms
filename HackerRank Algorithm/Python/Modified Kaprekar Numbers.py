63a4babdd52cc197f824c2f5a92076eb6db6eede
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-09-04 22:42:25 +0600

    Modified Kaprekar Numbers

---
+++ HackerRank Algorithm/Python/Modified Kaprekar Numbers.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+count = 0
+for i in range(int(input()),int(input())+1):
+    s = str(i**2)
+    x = s[len(s)//2:]
+    y = s[0:len(s)//2]
+    if x=='':x=0
+    if y=='':y=0
+    if int(x)+int(y)==i:print(i,end=' ');count+=1
+if count==0:print("INVALID RANGE")

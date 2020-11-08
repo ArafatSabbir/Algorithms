567cac8abda4e2f2207f0f511c534b64258d92a7
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-11-08 11:10:24 +0600

    Create Target Array

---
+++ LeetCode/Create Target Array.py
@@ -0,0 +1,13 @@
+#  Runtime: 36 ms, faster than 35.58% of Python3 online submissions
+#  Memory Usage: 14.2 MB, less than 99.97% of Python3 online submissions
+
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        result = []
+
+        for i in range(len(nums)):
+            result[index[i]:index[i]] = [nums[i]]
+
+
+        return result

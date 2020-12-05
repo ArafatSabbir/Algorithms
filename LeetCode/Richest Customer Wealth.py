a60bfd749109c406efa4baa7552ee6e46c181062
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-05 19:52:31 +0600

    Update Richest Customer Wealth.py

--- LeetCode/Richest Customer Wealth.py
+++ LeetCode/Richest Customer Wealth.py
@@ -13,10 +13,25 @@ class Solution:
     # v2
     
     
-    class Solution:
+class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         max_wealth = 0
         for i in accounts:
             if sum(i) >= max_wealth:
                 max_wealth = sum(i)
         return max_wealth
+
+    
+#  Runtime: 88 ms, faster than 5.17% of Python3 online submissions for Richest Customer Wealth.
+#  Memory Usage: 14.2 MB, less than 84.58% of Python3 online submissions for Richest Customer Wealth.
+
+#----------------low memory one line
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(map(sum, accounts))
+    
+    
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max([sum(i) for i in accounts])

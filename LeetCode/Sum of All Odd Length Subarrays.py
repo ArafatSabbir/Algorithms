cd1ec3858e537e2920449280becdbb5695e3eade
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-10 19:41:11 +0600

    Sum of All Odd Length Subarrays less space

--- LeetCode/Sum of All Odd Length Subarrays.py
+++ LeetCode/Sum of All Odd Length Subarrays.py
@@ -14,3 +14,18 @@ class Solution:
                 if (j - i) % 2 == 0:
                     res += s
         return res
+
+
+    
+    
+# Runtime: 56 ms, faster than 73.34% of Python3 online submissions for Sum of All Odd Length Subarrays.
+# Memory Usage: 14.2 MB, less than 23.92% of Python3 online submissions for Sum of All Odd Length Subarrays.
+    
+class Solution:
+    def sumOddLengthSubarrays(self, arr):
+        n = len(arr)
+        res = 0
+        for l in range(1, n + 1, 2):
+            for i in range(n - l + 1):
+                res += sum(arr[i:i + l])
+        return res

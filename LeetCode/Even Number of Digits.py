495b152c5c8268d3f164d1ae307e902266866763
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-11 22:17:41 +0600

    Even Number of Digits.py

--- LeetCode/Even Number of Digits.py
+++ LeetCode/Even Number of Digits.py
@@ -10,3 +10,14 @@ class Solution:
             if len(str(i))%2 == 0:
                 count += 1 
         return count
+    
+    
+
+# Runtime: 44 ms, faster than 96.07% of Python3 online submissions for Find Numbers with Even Number of Digits.
+# Memory Usage: 14.5 MB, less than 17.82% of Python3 online submissions for Find Numbers with Even Number of Digits.
+    
+    
+    
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        return sum(len(str(i)) % 2 == 0 for i in nums)

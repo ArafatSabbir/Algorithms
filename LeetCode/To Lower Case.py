c4e59b964e3b67a5499774e3ad3e4736eb7d2791
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-11 22:00:45 +0600

    To Lower Case.

---
+++ LeetCode/To Lower Case.py
@@ -0,0 +1,8 @@
+# Runtime: 24 ms, faster than 89.72% of Python3 online submissions for To Lower Case.
+# Memory Usage: 14.2 MB, less than 37.55% of Python3 online submissions for To Lower Case.
+
+
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()

2c10291afcb4a8e3d3241458d4c24e402b116f33
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-12 21:48:56 +0600

    Maximum Product of Two Elements in an Array.

---
+++ LeetCode/aximum Product of Two Elements in an Array.py
@@ -0,0 +1,13 @@
+# Runtime: 1012 ms, faster than 5.12% of Python3 online submissions for Maximum Product of Two Elements in an Array.
+# Memory Usage: 14.3 MB, less than 45.49% of Python3 online submissions for Maximum Product of Two Elements in an Array.
+
+
+class Solution:
+    def maxProduct(self, numbers: List[int]) -> int:
+        n = len(numbers)
+        max_product = 0
+        for first in range(n):
+            for second in range(first + 1, n):
+                max_product = max(max_product,(numbers[first]-1) * (numbers[second]-1))
+
+        return max_product

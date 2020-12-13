6598bb2469d280de24cf3b9e4339079cf2cda42d
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-13 19:51:27 +0600

    Count of Matches in Tournament.

---
+++ LeetCode/Count of Matches in Tournament.cs
@@ -0,0 +1,12 @@
+
+// Runtime: 40 ms, faster than 100.00% of C# online submissions for Count of Matches in Tournament.
+// Memory Usage: 15 MB, less than 100.00% of C# online submissions for Count of Matches in Tournament.
+
+
+
+public class Solution {
+    public int NumberOfMatches(int n) {
+        return n-1;
+        
+    }
+}

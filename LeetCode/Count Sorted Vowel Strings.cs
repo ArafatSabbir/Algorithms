6eb83665a7fd59f392b420a384238baca9fe00d8
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-22 22:03:50 +0600

    Count Sorted Vowel Strings.

---
+++ LeetCode/Count Sorted Vowel Strings.cs
@@ -0,0 +1,11 @@
+// Runtime: 36 ms, faster than 94.23% of C# online submissions for Count Sorted Vowel Strings.
+
+
+
+
+public class Solution {
+    public int CountVowelStrings(int n) {
+        return ((n + 1) * (n + 2) * (n + 3) * (n + 4)) / 24;
+        
+    }
+}

1f47bd021d9932b46b279f41000817342bd26365
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-12-24 22:41:37 +0600

    Queries on a Permutation With Key update

--- LeetCode/Queries on a Permutation With Key.cs
+++ LeetCode/Queries on a Permutation With Key.cs
@@ -26,3 +26,33 @@ public class Solution {
      }
         
 }
+
+
+/// v2
+// Runtime: 236 ms, faster than 84.62% of C# online submissions for Queries on a Permutation With Key.
+// Memory Usage: 31.2 MB, less than 38.46% of C# online submissions for Queries on a Permutation With Key.
+
+public class Solution {
+   public int[] ProcessQueries(int[] q, int m)
+        {
+            int len = q.Length;
+            List<int> fnList = new List<int>();
+
+            List<int> li = new List<int>();
+            for (int i = 1; i <= m; i++)
+                li.Add(i);
+
+            foreach (var i in q)
+            {
+                int ind = li.IndexOf(i);
+                fnList.Add(ind);
+                li.RemoveAt(ind);
+                li.Insert(0, i);
+
+            }
+
+            return fnList.ToArray();
+        }
+        
+}
+

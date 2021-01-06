abaeb06f079e916fa0226d1f1603908a3164deeb
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2021-01-06 20:07:12 +0600

    Reverse Words in a String III with SB

--- LeetCode/Reverse Words in a String III.cs
+++ LeetCode/Reverse Words in a String III.cs
@@ -5,3 +5,20 @@ public class Solution {
         return string.Join(" ",s.Split(' ').Select(str => new String(str.Reverse().ToArray())));
     }
 }
+
+// String builder
+
+public class Solution {
+    public string ReverseWords(string s) {
+        string[] split = s.Split(" ");
+		StringBuilder sb = new StringBuilder();
+		foreach (string w in split){
+			char[] arr = w.ToCharArray();
+			Array.Reverse(arr);
+			sb.Append(new string(arr));
+			sb.Append(" ");
+		}
+
+		return sb.ToString().Trim();
+    }
+}

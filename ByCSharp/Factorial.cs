604e68329c38459d4dc7f143992acafbb1eb67df
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-01-07 19:38:57 +0600

    Factorial by Recursion

--- ByCSharp/Factorial.cs
+++ ByCSharp/Factorial.cs
@@ -8,12 +8,13 @@ namespace ByCSharp
     {
         static void Main(string[] args)
         {
-            int n = Int32.Parse(Console.ReadLine());
-            Console.WriteLine(n + " Factorial is : " + FactorialByLoop(n));
+           int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(n + " Factorial is : " + FactorialByRecursion(n));
         }
-
+        
         private static int FactorialByLoop(int num)
         {
+            
             int fac = 1;
             
             for (int i = 1; i <= num; i++)
@@ -24,5 +25,13 @@ namespace ByCSharp
             return fac;
         }
 
+        private static int FactorialByRecursion(int num)
+        {
+            int fac = 1;
+
+            if (num > 0)
+                fac = num * FactorialByRecursion(num - 1);
+            return fac;
+        }
     }
 }

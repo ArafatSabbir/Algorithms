21031c16d43e570af952d97527c6f9a80939e71d
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-01-07 18:41:23 +0600

    Fibonacci Sum

---
+++ ByCSharp/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci obj = new Fibonacci();
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Sum is : " + obj.FibSum(n));
+        }
+
+        int FibSum(int num)
+        {
+            if (num <= 1)
+                return num;
+
+            return FibSum(num - 1) + FibSum(num - 2);
+        }
+    }
+}

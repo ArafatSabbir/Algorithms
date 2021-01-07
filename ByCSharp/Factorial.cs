0865f05aa053b5db6a758504cbd670d53b3833d7
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-01-07 19:25:19 +0600

    Factorial by loop

---
+++ ByCSharp/Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(n + " Factorial is : " + FactorialByLoop(n));
+        }
+
+        private static int FactorialByLoop(int num)
+        {
+            int fac = 1;
+            
+            for (int i = 1; i <= num; i++)
+            {
+                fac = fac * i;
+            }
+
+            return fac;
+        }
+
+    }
+}

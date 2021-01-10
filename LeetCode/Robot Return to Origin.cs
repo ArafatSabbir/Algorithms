72317d263d6309f5bc32e92bdd4225887e9edd6a
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2021-01-10 22:18:04 +0600

    Robot Return to Origin

--- LeetCode/Robot Return to Origin.cs
+++ LeetCode/Robot Return to Origin.cs
@@ -4,3 +4,39 @@ public class Solution {
         
     }
 }
+
+
+// fast
+
+public class Solution {
+    public bool JudgeCircle(string moves) {
+        int h = 0;
+        int v = 0;
+        char[] m = moves.ToCharArray();
+        
+        for(int i = 0; i < m.Length; i++)
+        {
+            switch (m[i])
+            {
+                case 'L':
+                    h--;
+                    break;
+                case 'R':
+                    h++;
+                    break;
+                case 'U':
+                    v++;
+                    break;
+                case 'D':
+                    v--;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        
+        return (h == 0 && v == 0);
+        
+    }
+}

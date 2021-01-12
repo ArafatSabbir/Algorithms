8d1201dec23b7e54865eba6de25745b43172ea87
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-01-12 12:33:09 +0600

    SumOfDigits

---
+++ ByCSharp/SumOfDigits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class SumOfDigits
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(sumOfDisit(n));
+        }
+
+        static int sumOfDisit(int num)
+        {
+            int sum = 0, dupnum = num, reminder = 0;
+
+            while (num != 0)
+            {
+                reminder = num % 10;
+                num = num / 10;
+                sum = sum + reminder;
+            }
+
+            return sum;
+        }
+    }
+}

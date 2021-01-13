f073fd838bc7b35c12b426ad796a8f6ccecdf9bb
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-01-13 21:40:50 +0600

    SumOfnNaturalNum

---
+++ ByCSharp/SumOfnNaturalNum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ByCSharp
+{
+    class SumOfnNaturalNum
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(SumOfNaturalNum(n));
+        }
+
+        static int SumOfNaturalNum(int number)
+        {
+            int sum = 0;
+            sum = (number*(number + 1)) / 2;
+            return sum;
+        }
+    }
+}

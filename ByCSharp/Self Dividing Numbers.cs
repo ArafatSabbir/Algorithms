ac0804cb6d81418c603396ba7bbdc42cfeb5829b
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-02-14 17:14:43 +0600

    SumOfUnique Numbers

---
+++ ByCSharp/Self Dividing Numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    
+    class Self_Dividing_Numbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SelfDividingNumbers(2,20));
+        }
+
+
+        public static bool IsSelfDivisible(int n)
+        {
+            foreach (char c in n.ToString())
+                if (c == '0' || n % (c - '0') != 0)
+                {
+                    return false;
+                }
+            return true;
+        }
+
+        public static List<int> SelfDividingNumbers(int left, int right)
+        {
+            List<int> result = new List<int>();
+            for (int i = left; left <= right; i++)
+                if (IsSelfDivisible(i))
+                {
+                    result.Add(i);
+                }
+            return result;
+        }
+    }
+
+   
+}
---
+++ ByCSharp/SumOfUniqueNums.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class SumOfUniqueNums
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] nums = {1, 2, 2, 3, 4, 5};
+            if (nums == null || nums.Length == 0)
+                 sum = 0;
+
+            int[] arr = new int[100];
+            foreach (int num in nums)
+            {
+                arr[num - 1]++;
+            }
+
+            foreach (int num in arr)
+            {
+                Console.WriteLine(num);
+            }
+
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 1)
+                    sum += i + 1;
+            }
+            Console.WriteLine(sum);
+        }
+        public static int SumOfUnique(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+                return 0;
+
+            int[] arr = new int[100];
+            foreach (int num in nums)
+            {
+                arr[num - 1]++;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 1)
+                    sum += i + 1;
+            }
+
+            return sum;
+        }
+    }
+}

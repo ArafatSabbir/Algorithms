0b5df99c6b51235de69699fa4eccdafe3947ce1d
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-02-15 18:28:01 +0600

    Factors Of a Given Number

---
+++ ByCSharp/FactorOfaGivenNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class FactorOfaGivenNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number To find it's Factors");
+            int num = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num%i == 0)
+                {
+                    Console.WriteLine($"{i} is Factor of {num}");
+                }
+            }
+        }
+
+
+
+    }
+}

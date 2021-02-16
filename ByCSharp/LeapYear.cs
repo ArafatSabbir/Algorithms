71062b9145b7f4b51218bbccf69dba6990c93f89
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-02-16 15:40:22 +0600

    Leap Year

---
+++ ByCSharp/LeapYear.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Year");
+            int num = Int32.Parse(Console.ReadLine());
+            if (IsLeapYear(num))
+            {
+                Console.WriteLine($"{num} is Leap Year");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not Leap Year");
+            }
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else if (year % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+}

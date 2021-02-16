7cf85b8c65f5d9a98a1478eda9d67610f6abe385
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-02-16 16:34:03 +0600

    Single Number

---
+++ ByCSharp/SingleNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ByCSharp
+{
+    class SingleNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 2, 3, 4, 3, 2, 4, 5, 1, 3, 1};
+            Console.WriteLine(singleNumber(nums));
+            
+        }
+
+        public static int singleNumber(int[] nums)
+        {
+            int n = 0;
+            foreach (int num in nums)
+            {
+                n ^= num;
+            }
+            return n;
+        }
+    }
+}

c50fcba1aa6e59c397f50159fda9bc8f45cd028f
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-02-25 20:41:46 +0600

    Merge Strings Alternately

---
+++ ByCSharp/MergeStringsAlternately.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ByCSharp
+{
+    class MergeStringsAlternately
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            string result = "";
+            int i = 0, j = 0;
+            while (i < word1.Length || j < word2.Length)
+            {
+
+                if (i < word1.Length)
+                    result = result + word1[i];
+
+                if (j < word2.Length)
+                    result = result + word2[j];
+
+                i++;
+                j++;
+            }
+            return result;
+        }
+    }
+}

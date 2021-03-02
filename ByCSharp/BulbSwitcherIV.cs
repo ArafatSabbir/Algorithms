995ce5b5b0ed4c03cca4c1fe8f9df36129e4da13
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-02 19:25:54 +0600

    Bulb Switcher IV

---
+++ ByCSharp/BulbSwitcherIV.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class BulbSwitcherIV
+    {
+        static bool allCharactersSame(string s)
+        {
+            int n = s.Length;
+            for (int i = 1; i < n; i++)
+                if (s[i] != s[0])
+                    return false;
+
+            return true;
+        }
+
+        public int MinFlips(string target)
+        {
+            int count = 0;
+
+            if (allCharactersSame(target))
+            {
+                if (target[0] == '0')
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            for (int i = 1; i < target.Length; i++)
+            {
+                
+                if (target[i - 1] != target[i])
+                {
+                    count++;
+                } 
+
+            }
+
+            if (target[0] == '0')
+            {
+                return count - 1 ;
+            }
+
+
+            return count;
+
+        }
+    }
+}

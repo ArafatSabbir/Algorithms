215afaff13ea1c3bbf7e6716a2b671bd4f328292
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-03 20:42:41 +0600

    Closest Dessert Cost

---
+++ ByCSharp/ClosestDessertCost.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class ClosestDessertCost
+    {
+        public int ClosestCost(int[] baseCosts, int[] toppingCosts, int target)
+        {
+            bool[] dp = new bool[30000];
+            dp[0] = true;
+            foreach (var t in toppingCosts)
+            {
+                for (int i = 29999; i >= 0; i--)
+                {
+                    if (dp[i])
+                    {
+                        if (i + t < 30000) dp[i + t] = true;
+                        if (i + 2 * t < 30000) dp[i + 2 * t] = true;
+                    }
+                }
+            }
+            int best = 99999999;
+            foreach (var b in baseCosts)
+            {
+                for (int i = 0; i < 30000; i++)
+                {
+                    if (dp[i])
+                    {
+                        int v = b + i;
+                        if (Math.Abs(v - target) < Math.Abs(best - target) ||
+                            Math.Abs(v - target) == Math.Abs(best - target) && v < best
+                        )
+                        {
+                            best = v;
+                        }
+                    }
+                }
+            }
+            return best;
+        }
+	}
+}

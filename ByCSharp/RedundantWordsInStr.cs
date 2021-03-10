d213ac886651c97bf6db36cbb4f5354961201751
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-10 22:14:31 +0600

    Redundant Words In String

---
+++ ByCSharp/RedundantWordsInStr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class RedundantWordsInStr
+    {
+        public IList<string> FindDuplicatesStr(string s)
+        {
+            string[] words = s.Split(' ');
+            var set = new HashSet<string>();
+            var result = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (set.Contains(word))
+                    result.Add(word);
+
+                set.Add(word);
+            }
+
+            return result;
+        }
+    }
+}

8bc57abc73a51e4651731e160773b72b5a028c89
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-10 11:49:05 +0600

    Find all duplicates in array

---
+++ ByCSharp/DuplicateInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace ByCSharp
+{
+    class DuplicateInArray
+    {
+        public IList<int> FindDuplicates(int[] nums)
+        {
+            var res = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var index = Math.Abs(nums[i])-1;
+                if (nums[index] < 0)
+                {
+                    res.Add(index+1);
+                }
+
+                nums[index] = -nums[index];
+            }
+
+            return res;
+
+        }
+    }
+}
---
+++ ByCSharp/TestPlace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ByCSharp
+{
+    class TestPlace
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "bdca";
+            List<char> sw1 = s1.ToCharArray().ToList().Sort();
+            foreach (var i in s1)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}

9506647593b9a6421227b440a01010eb343847dd
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-10 17:14:02 +0600

    Disits In Numer

---
+++ ByCSharp/DigitsInNum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    public static class DigitsInNum
+    {
+        public static void DisitsInNumer(int num)
+        {
+            if (num == 0) return;
+            int disit = num % 10;
+            DisitsInNumer(num / 10);
+            Console.Write($"{disit} ");
+        }
+
+
+    }
+}

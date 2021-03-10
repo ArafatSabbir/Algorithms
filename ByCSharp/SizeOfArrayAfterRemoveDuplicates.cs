48de942094cc976915b50f4b784df32b73f80646
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-10 22:06:32 +0600

    Size Of Array After Remove Duplicates

---
+++ ByCSharp/SizeOfArrayAfterRemoveDuplicates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class SizeOfArrayAfterRemoveDuplicates
+    {
+        public int AfterDuplicateRemove(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+            return set.Count;
+        }
+    }
+}

039b5482d2813e439f8c5e56b5d20902586792f0
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-10 17:57:31 +0600

    Abbreviation Check

---
+++ ByCSharp/AbbreviationCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class AbbreviationCheck
+    {
+        public static bool AbbrCheck(string str, string abr)
+        {
+            string s = "";
+            string[] words = s.Split(' ');
+            foreach (var w in words)
+            {
+                s += w[0];
+            }
+
+            if (s == abr)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

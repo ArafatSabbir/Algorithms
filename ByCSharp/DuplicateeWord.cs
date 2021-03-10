24cdca2dfa975e402b665eecc3192135e7a99d69
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-10 22:00:52 +0600

    Duplicate Word Check

---
+++ ByCSharp/DuplicateeWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class DuplicateeWord
+    {
+        public static bool DuplicateWordCheck(string str)
+        {
+            string[] words = str.Split(' ');
+            var hs = new HashSet<string>(words);
+            if (words.Length == hs.Count)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}

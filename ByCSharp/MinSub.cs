990f6e65a57924fce067478f3b1abf6a469bd403
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-11 11:43:32 +0600

    Find x and y such that x * y == n and abs(x-y)

---
+++ ByCSharp/MinSub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    static class MinSub
+    {
+        public static int[] MinimumDistance(int n)
+        {
+            int[] xy = new int[2];
+            int temp = (int)Math.Sqrt(n);
+            for (int i = temp; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    xy[0] = i;
+                    xy[1] = n / xy[0];
+                    break;
+                }
+            }
+            return xy;
+        }
+    }
+}

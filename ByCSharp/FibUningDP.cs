29e6f05c6d9e4f5fdd46041a2421bd8537d6bea1
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-03-16 14:12:16 +0600

    Fibonacchi uning DP

---
+++ ByCSharp/FibUningDP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class FibUningDP
+    {
+        public static int Fib(int n)
+        {
+            int[] seq = new int[n];
+            seq[0] = seq[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                seq[i] = seq[i - 1] + seq[i - 2];
+            }
+
+            return seq[n - 1];
+        }
+    }
+}

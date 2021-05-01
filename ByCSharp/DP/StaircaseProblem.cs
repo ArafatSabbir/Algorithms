2543f2777daea553ad30f2372f8df7ce7c9f6d5a
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-05-01 23:00:04 +0600

    Stair Case 3,2,1 problem

---
+++ ByCSharp/DP/StaircaseProblem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp.DP
+{
+    class StaircaseProblem
+    {
+        //static void Main(string[] args)
+        //{
+        //    int t = Int32.Parse(Console.ReadLine());
+        //    for (int i = 0; i < t; i++)
+        //    {
+        //        var o = new StaircaseProblem();
+        //        int n = Int32.Parse(Console.ReadLine());
+        //        Console.WriteLine(o.Stair(n));
+        //    }
+
+        //}
+
+        int Stair(int n)
+        {
+            if (n==1 || n==2 )
+            {
+                return n;
+            }
+            if ( n == 3)
+            {
+                return 4;
+            }
+
+            int res = 0;
+
+            res += Stair(n - 1);
+            res += Stair(n - 2);
+            res += Stair(n - 3);
+
+            return res;
+        }
+    }
+}

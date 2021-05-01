be27397919e11d0b05b57ea9edde14805780274d
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-05-01 16:22:00 +0600

    Covid_19_icpc_preli

---
+++ ByCSharp/ICPC_DHAKA_Preli_2020/covid_19_j.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp.ICPC_DHAKA_Preli_2020
+{
+    class covid_19_j
+    {
+        public static void Main(string[] args)
+        {
+            int t = Console.Read();
+            int result = 0;
+            if (t < 6)
+            {
+                result = 1;
+            }
+            else
+            {
+                if (t % 6 == 0)
+                {
+                    result = (t / 6) + 1;
+                }
+                else
+                {
+                    result = (t / 6) + 1;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}

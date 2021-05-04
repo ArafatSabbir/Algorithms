78cfe23b75e951ac72cf240464488e82eea08042
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-05-05 01:27:30 +0600

    BS pblms

---
+++ ByCSharp/BS23/BoardGames.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp.BS23
+{
+    class BoardGames
+    {
+        //public static void Main(string[] args)
+        //{
+        //    int t = Int32.Parse(Console.ReadLine());
+        //    int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        //    boardGames(t, arr);
+        //}
+
+        public static void boardGames(int n, int[] arr)
+        {
+
+            int i, num0 = 0, num5 = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    num0++;
+                }
+                else
+                {
+                    num5++;
+                }
+            }
+
+            num5 = (num5 / 9) * 9;
+
+            if (num0 == 0)
+            {
+                Console.Write(-1);
+            }
+
+            else if (num5 == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                for (i = 0; i < num5; i++)
+                {
+                    Console.Write(5);
+                }
+                for (i = 0; i < num0; i++)
+                {
+                    Console.Write(0);
+                }
+            }
+        }
+
+    }
+}
---
+++ ByCSharp/BS23/BrainStation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+namespace ByCSharp.BS23
+{
+    class BrainStation
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int t = Int32.Parse(Console.ReadLine());
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                bs(arr);
+
+            }
+            catch (Exception)
+            {
+            }
+
+            
+        }
+
+        public static void bs(int[] arr)
+        {
+            //Array.Sort(arr);
+            List<List<int>> li= new List<List<int>>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    for (int k = 0; k < arr.Length; k++)
+                    {
+                        List<int> tm = new List<int>();
+                        if (((arr[i]<arr[j] && arr[j]%arr[i] ==0 )&& (arr[j]<arr[k] && arr[k]%arr[j] == 0)))
+                        {
+                            tm.Append(arr[i]);
+                            tm.Append(arr[j]);
+                            tm.Append(arr[k]);
+                        }
+
+                        if (tm.Count != 0)
+                        {
+                            li.Append(tm);
+                        }
+
+                    }
+                }
+                
+            }
+
+            if (li.Count == 0)
+            {
+                Console.WriteLine(-1);
+            }
+
+            else
+            {
+                foreach (var i in li)
+                {
+                    foreach (var si in i)
+                    {
+                        Console.WriteLine(si);
+                    }
+                }
+            }
+
+
+        }
+    }
+}

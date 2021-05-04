a20f4d7739b541ddc21728a64e3a13b86df8c098
ArafatSabbir <sabbirahmedcse33@gmail.com>
2021-05-05 01:26:19 +0600

    Majority Element

---
+++ ByCSharp/MajorityElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByCSharp
+{
+    class MajorityElement
+    {
+        public int majorityElement(int[] nums)
+        {
+            int n = nums.Length;
+            Dictionary<int, int> count = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (count.ContainsKey(nums[i]))
+                    count[nums[i]]++;
+                else
+                    count.Add(nums[i], 1);
+                if (count[nums[i]] > n / 2) return nums[i];
+            }
+            return Int32.MaxValue;
+        }
+    }
+}

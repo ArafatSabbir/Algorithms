295708ea4e9c4dbe205ace4fddbb9553c233beba
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2021-06-13 18:27:31 +0600

    Number of Points Inside a Circle.

--- LeetCode/Number of Points Inside a Circle.cs
+++ LeetCode/Number of Points Inside a Circle.cs
@@ -3,19 +3,16 @@
 
 public class Solution {
     public int[] CountPoints(int[][] points, int[][] queries) {
-        int qlen = queries.Length;
-        int[] res = new int[qlen];
+        int[] res = new int[queries.Length];
         
-        for(int i = 0; i<qlen;i++){
-            int pCount = 0;
+        for(int i = 0; i<queries.Length;i++){
+            res[i] = 0;
             for(int p = 0; p < points.Length; p++){
                 if(( Math.Pow((points[p][0]-queries[i][0]), 2) + Math.Pow((points[p][1]-queries[i][1]), 2)) <= Math.Pow(queries[i][2], 2)){
-                    pCount++;
+                    res[i]++;
                 }
-            }
-            res[i] = pCount;
-        } 
-        
+            }  
+        }  
         return res;
     }
 }
